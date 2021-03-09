6156ee442bff3b70e22c6d24bdda926d09b2e11f
Jillian Leblow <jillianleblow@gmail.com>
2021-03-08 19:29:49 -0700

    first commit - version 1 of dice duel

---
+++ dicee.js
@@ -0,0 +1,49 @@
+function refreshPage(){
+    window.location.reload();
+}
+
+let diceRoll = () => {
+  let player1Roll = Math.floor(Math.random() * 6) + 1;
+  let player2Roll = Math.floor(Math.random() * 6) + 1;
+
+  console.log(player1Roll);
+  console.log(player2Roll);
+
+  if (player1Roll > player2Roll) {
+    document.querySelector("#winner-text").innerHTML = "<i class='fas fa-flag'></i> Player 1 Wins!";
+  } else if (player1Roll < player2Roll) {
+    document.querySelector("#winner-text").innerHTML = "Player 2 Wins! <i class='fas fa-flag'></i>";
+  } else if (player1Roll === player2Roll) {
+    document.querySelector("#winner-text").innerHTML = "Draw!";
+  }
+
+  if (player1Roll === 1) {
+    document.querySelector(".p1-dice").src = "images/png/1y.png";
+  } else if (player1Roll === 2) {
+    document.querySelector(".p1-dice").src = "images/png/2y.png";
+  } else if (player1Roll === 3) {
+    document.querySelector(".p1-dice").src = "images/png/3y.png";
+  } else if (player1Roll === 4) {
+    document.querySelector(".p1-dice").src = "images/png/4y.png";
+  } else if (player1Roll === 5) {
+    document.querySelector(".p1-dice").src = "images/png/5y.png";
+  } else if (player1Roll === 6) {
+    document.querySelector(".p1-dice").src = "images/png/6y.png";
+  }
+
+  if (player2Roll === 1) {
+    document.querySelector(".p2-dice").src = "images/png/1y.png";
+  } else if (player2Roll === 2) {
+    document.querySelector(".p2-dice").src = "images/png/2y.png";
+  } else if (player2Roll === 3) {
+    document.querySelector(".p2-dice").src = "images/png/3y.png";
+  } else if (player2Roll === 4) {
+    document.querySelector(".p2-dice").src = "images/png/4y.png";
+  } else if (player2Roll === 5) {
+    document.querySelector(".p2-dice").src = "images/png/5y.png";
+  } else if (player2Roll === 6) {
+    document.querySelector(".p2-dice").src = "images/png/6y.png";
+  }
+}
+
+diceRoll();
